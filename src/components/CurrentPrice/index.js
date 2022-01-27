da3fc092d3d31f2010cb8168b5b7fd206d675ced
Moabe <moabeluis2018@gmail.com>
2022-01-26 22:26:42 -0300

    index CurrentPrice começando

---
+++ src/components/CurrentPrice/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native"
+import styles from "./styles"
+
+export default function CurrentPrice(){
+    return(
+        <View>
+            <Text>$ 54423.355</Text>
+            <Text>Ultima cotação</Text>
+        </View>
+    )
+}
