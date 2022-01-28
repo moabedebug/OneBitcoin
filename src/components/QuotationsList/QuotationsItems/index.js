dbc4ad50095375566d62e688700841bf368223d2
Moabe <moabeluis2018@gmail.com>
2022-01-27 22:14:35 -0300

    estilo finalizado aula04

--- src/components/QuotationsList/QuotationsItems/index.js
+++ src/components/QuotationsList/QuotationsItems/index.js
@@ -5,18 +5,18 @@ import styles from "./style"
 export default function QuotationsItems(){
     return(
         <View style={styles.mainContent}>
-            <View style={styles.contextLeft}>
-                <View style={styles.boxLogo}>
-                    <Image
-                        source={require("../../../img/bitcoin.png")}
-                        style={styles.logoBitcoin}
-                    />
-                    <Text style={styles.dataCotation}>27/01/2022</Text>
-                </View>
-                <View style={styles.contextRight}>
-                    <Text style={styles.price}>$ 53331.052</Text>
-                </View>
+          <View style={styles.contextLeft}>
+            <View style={styles.boxLogo}>
+              <Image
+                source={require("../../../img/bitcoin.jpg")}
+                style={styles.logoBitcoin}
+              />
+                <Text style={styles.dayCotation}>27/01/2022</Text>
             </View>
+          </View>
+          <View style={styles.contextRight}>
+            <Text style={styles.price}>$ 53331.052</Text>
+          </View>
         </View>
     )
 }
--- src/components/QuotationsList/QuotationsItems/style.js
+++ src/components/QuotationsList/QuotationsItems/style.js
@@ -4,13 +4,42 @@ const styles = StyleSheet.create({
     mainContent:{
         width: "95%",
         height: "auto",
-        backgroundColor: "#ffffff",
+        backgroundColor: "#000000",
         marginLeft: "3%",
         marginBottom: 15,
         borderRadius: 10,
         flexDirection: "row",
         alignItems: "center",
         padding: 10
+    },
+    contextLeft:{
+        width: "36%",
+        alignItems: "flex-start",
+
+    },
+    boxLogo:{
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    logoBitcoin: {
+        width: 40,
+        height: 40,
+        marginLeft: 2
+    },
+    dayCotation:{
+        fontWeight: "bold",
+        fontSize: 16,
+        paddingLeft: 5,
+        color: "#ffffff"
+    },
+    contextRight: {
+        width: "60%",
+        alignItems: "flex-end",
+    },
+    price: {
+        fontWeight: "bold",
+        fontSize: 18,
+        color: "#ffffff"
     }
 })
 
